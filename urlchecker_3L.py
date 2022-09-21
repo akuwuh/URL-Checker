67f124144aaf68477e463d87e2944ff63a63fec2
cute <60953696+akuwuh@users.noreply.github.com>
2022-09-21 13:00:11 -0400

    Update urlchecker_3L.py

--- urlchecker_3L.py
+++ urlchecker_3L.py
@@ -12,15 +12,20 @@ zebi = 'abcdefghijklmnopqrstuvwxyz'
 def pad_right(string, n_chars):
     return string + (' ' * (n_chars - len(string)))
 
+#initializing web browser search
 class Id:
     def __init__(self, url):
         req = request.Request(url, headers={'User-Agent':'Mozilla/5.0'})
         html = request.urlopen(req).read().decode('utf-8')
         self.exist = not 'No group could be retrieved for the given URL.' in html
 
+
+#Search algorithm
 def loop1():
     while True:
+        #concatenating base url with 3 random letters
         url = base_url + random.choice(zebi)+random.choice(zebi)+random.choice(zebi)
+        
         if not url in allchar:
             try:
                 allchar.append(url)
